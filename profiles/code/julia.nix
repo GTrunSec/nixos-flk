cdd161569c1b82457c8586b471213ac475da91ec
GTrunSec <gtrunsec@hardenedlinux.org>
2020-08-26 19:01:18 -0700

    overlays: julia 1.4.2 to 1.51

--- profiles/code/julia.nix
+++ profiles/code/julia.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }:
 let
   julia = pkgs.julia_13.overrideAttrs(oldAttrs: {
-   src = pkgs.fetchzip {
-     url = "https://github.com/JuliaLang/julia/releases/download/v1.4.2/julia-1.4.2-full.tar.gz";
-     sha256 = "14jghi9mw0wdi6y9saarf0nzary9i21jx43zznddzrq48v4nlayj";
-   };
-   checkTarget = "";
+    src = pkgs.fetchzip {
+      url = "https://github.com/JuliaLang/julia/releases/download/v1.5.1/julia-1.5.1-full.tar.gz";
+      sha256 = "sha256-uaxlzni2RtmDhMzPbtDycj44CB0tJUzhmbwsAGwFv/U=";
+    };
+    checkTarget = "";
   });
 
   cudaVersion = pkgs.cudatoolkit_10_2;
