079c46728467fa16c0f02ce90a40df20eed09737
GTrunSec <gtrunsec@hardenedlinux.org>
2020-09-14 16:21:56 -0700

    using user modules

--- modules/list.nix
+++ modules/list.nix
@@ -1,4 +1,3 @@
 [
   ./ids/zeek
-  ./promnesia
 ]
--- profiles/data/promnesia.nix
+++ profiles/data/promnesia.nix
@@ -1,8 +1,42 @@
 { config, lib, pkgs, ... }:
+let
+  readConfig = (builtins.readFile ./config.py);
+  configFile = pkgs.writeScript "config.py" readConfig;
+  watcherPath = "/home/gtrun/Dropbox/org-notes/GTD";
+  PreShell = pkgs.writeScript "preRun-promnesia" ''
+  if [ ! -d "$HOME/.local/share/promnesia.sqlite" ];then
+     ${pkgs.promnesia}/bin/promnesia index --config ${configFile}
+     fi
+    '';
+in
 {
-  services.promnesia = {
-    enable = true;
-    config = (builtins.readFile ./config.py);
-    watcherPath = "/home/gtrun/Dropbox/org-notes/GTD";
+  systemd.user.services.promnesia = {
+    description = "promnesia Daemon";
+    wantedBy = [ "graphical-session.target" ];
+    preStart = ''
+      ${pkgs.bash}/bin/bash ${PreShell}
+        '';
+    serviceConfig = {
+      ExecStart = ''
+        ${pkgs.promnesia}/bin/promnesia serve
+        '';
+      Restart = "on-failure";
+    };
+  };
+
+  systemd.user.paths.promnesia-watcher = {
+    wantedBy = [ "promnesia.service" ];
+    pathConfig = {
+      PathModified = watcherPath;
+      Unit = "promnesia-restarter.service";
+    };
+  };
+
+  systemd.user.services.promnesia-restarter = {
+    serviceConfig.Type = "oneshot";
+    wantedBy = [ "promnesia.service" ];
+    script = ''
+      systemctl --user restart promnesia.service
+         '';
   };
 }
