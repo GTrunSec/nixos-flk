422497ab5c2e9e97d6c23b410a5649964089d75c
GTrunSec <gtrunsec@hardenedlinux.org>
2020-10-19 19:20:19 -0700

    overlay: update brave

--- overlays/brave.nix
+++ overlays/brave.nix
@@ -1,11 +1,11 @@
-let version = "1.12.108";
+let version = "1.15.75";
 in
 final: prev: {
   brave = prev.brave.overrideAttrs (o: {
     inherit version;
     src = prev.fetchurl {
       url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-browser_${version}_amd64.deb";
-      sha256 = "sha256-4SFNjYfDEwbIQAW7QMygpFQzjoJS6033LyOUfUj6osg=";
+      sha256 = "sha256-z6NXS8VPT2KCF/Nh7eirp5GVu2KlGq5cGv2InvAo7Ds=";
     };
   });
 }
