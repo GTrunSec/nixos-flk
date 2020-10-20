ae8eedd235377fd4ec2ef0e87ef810719c4881d9
GTrunSec <gtrunsec@hardenedlinux.org>
2020-10-19 18:56:24 -0700

    update polar-bookshelf

--- overlays/polar-bookshelf.nix
+++ overlays/polar-bookshelf.nix
@@ -1,11 +1,27 @@
-let version = "1.100.13";
+let version = "2.0.42";
 in
 final: prev: {
-  polar-bookshelf = prev.polar-bookshelf.overrideAttrs (o: {
+  polar-bookshelf = prev.polar-bookshelf.overrideAttrs (o: rec{
     inherit version;
     src = prev.fetchurl {
-      url = "https://github.com/burtonator/polar-bookshelf/releases/download/v${version}/polar-bookshelf-${version}-amd64.deb";
-      sha256 = "0i60np4akhvzqpspg5k2mz8q4ccmz19j43x1sisbdw7iml1a1rsd";
+      url = "https://github.com/burtonator/polar-bookshelf/releases/download/v${version}/polar-desktop-app-${version}-amd64.deb";
+      sha256 = "sha256-JyO71wyE6b0iHAYs/6/WbG+OdUVUUPpJla+ZUzg0Gng=";
     };
+
+    installPhase = ''
+    mkdir -p $out/share/polar-bookshelf
+    mkdir -p $out/bin
+    mkdir -p $out/lib
+
+    mv opt/Polar/* $out/share/polar-bookshelf
+    mv $out/share/polar-bookshelf/*.so $out/lib
+
+    mv usr/share/* $out/share/
+
+    ln -s $out/share/polar-bookshelf/polar-desktop-app.bin $out/bin/polar-desktop-app
+
+    substituteInPlace $out/share/applications/polar-desktop-app.desktop \
+      --replace "/opt/Polar/polar-desktop-app" "$out/bin/polar-desktop-app"
+    '';
   });
 }
