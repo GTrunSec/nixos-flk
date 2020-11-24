dc038c49bb3f42e705383d8e3804e8e46e30f19f
GTrunSec <gtrunsec@hardenedlinux.org>
2020-11-23 18:02:08 -0800

    update miniflux

--- profiles/data/miniflux.nix
+++ profiles/data/miniflux.nix
@@ -1,7 +1,8 @@
 { config, lib, pkgs, ... }:
-
 {
   services.miniflux = {
     enable = true;
+    adminCredentialsFile = ../../secrets/miniflux-admin-credentials;
+    config.LISTEN_ADDR = "localhost:9000";
   };
 }
