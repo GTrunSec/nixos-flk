5bebfd07c5b987e5a861fb429de77f574d05112f
GTrunSec <gtrunsec@hardenedlinux.org>
2020-11-28 00:40:03 -0800

    adguardhome: update

--- profiles/network/adguardhome.nix
+++ profiles/network/adguardhome.nix
@@ -1,9 +1,16 @@
 { config, lib, pkgs, ... }:
 
 {
+  networking.firewall = {
+    allowedTCPPorts = [ 9002 ];
+    allowedUDPPorts = [ 9002 ];
+  };
+
   services.adguardhome = {
     enable = true;
+    listenAddress = "10.220.170.112";
   };
+
   environment.systemPackages = with pkgs; [
     adguardhome
   ];
