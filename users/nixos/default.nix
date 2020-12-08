a38ed328ae19534cf566217c40c5b3ef5b77fd5a
GTrunSec <gtrunsec@hardenedlinux.org>
2020-12-07 16:50:12 -0800

    ssh: public key update

--- users/nixos/default.nix
+++ users/nixos/default.nix
@@ -11,7 +11,7 @@
     shell = "/run/current-system/sw/bin/zsh";
     uid = 1000;
     openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCkSgUxROiIN3k9lBRTXSYqUpHEv+KFTlWhBu4OTASs5sEUtbvUhpn2zeySTNb2SRbFsbECLTmfT2avDC2MeNnbivLAyEEAjSgLcpiwfVithUl2Tx59mFVh2wZs/4jyOQ5uX96kvutiWNb91+kQ27lUXk+3e15mgnKmQTyikENbsqzt79lRqjzj9V0KyuvbccDCcE9ZBSZ9LTKAhelYSZ1PwavsEBdXDZZBBVsRrxyVr1QueitdS3D4bxSBHuwwDHkXK3c/f5GPaYkh6Xnv/RzQkKB2dGu/ulw2up5pLShmGir9PFbhDoPfF/EzE9PbSSZV4EsHhLUu4tRsg4LyBtlN gtrun@GTruN"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC4pdjuq8ISTfHkjP/Kqd6lopZbYOeQqe4JvOlxHHoTZs8eqpdol3uCUszcEQZ/7W7pqes00k1n9R3AqkNssiS6ywtXC9UelIHbPkPoevBMYq8ct5+AZKB6NBEeMMQxEEa9m2UXKTnjr7KMn3kq9CglKo1YJA8gQ0GCuXwkpGxHInY8ea6fwSfCu7CuFNzKkAe+sMm9nrLe6SDkS//UZHuSPBRXyCUHkILx7vDq1g+yFvt4d0DwP2TkJjHWwbnfnBrMxWLwIEBnKGj0C5bnnLiXW3YX/ufEf/S2i5neF2Rh0e7zQAnwpAIVurt3tRiNYzK/araa5NKKXQbLotgfp9lveTjPL1dFusPKUDjtQdTRXza5E2QnHZeRuuKgT4MxMlXfJO2TrJxESTuebbom6w2dcVAxb/WQJr8GpvUhfYVwg2vRua9al72gQnFNlnM8AoJiPdojAV+1J51U5gHEyNUudUV6xRePz5zl1JZIC0Yxe0U+5O3F2RgzWe4w78xzAzk= gtrun@MacBook-Pro.attlocal.net"
     ];
   };
   services.lorri.enable = true;
