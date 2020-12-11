6f569531fe8fa9bec242062e1cb66929046a1c91
GTrunSec <gtrunsec@hardenedlinux.org>
2020-12-11 02:09:57 -0800

    update

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -1,11 +1,11 @@
-let version = "0.0.12";
+let version = "0.0.13";
 in
 final: prev: {
   discord = prev.discord.overrideAttrs (o: {
     inherit version;
     src = prev.fetchurl {
       url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
-      sha256 = "sha256-aPLNPDl7XFSxWwjw2HPhTladoCSxLC5WWF+hyxDbP2M=";
+      sha256 = "sha256-/uyoNTFgfuwaYjGtjquIu/GGXDnPX4K4hOO1JBczvzQ= ";
     };
   });
 }
