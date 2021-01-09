ba3bd4318e71939373d6bd9f290488da8c03f251
GTrunSec <gtrunsec@hardenedlinux.org>
2021-01-08 17:51:03 -0800

    brave:Bumpup

--- overlays/brave.nix
+++ overlays/brave.nix
@@ -1,11 +1,11 @@
-let version = "1.16.76";
+let version = "1.18.77";
 in
 final: prev: {
   brave = prev.brave.overrideAttrs (o: {
     inherit version;
     src = prev.fetchurl {
       url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-browser_${version}_amd64.deb";
-      sha256 = "sha256-K16wXOVVpIZjBSFNUkVUuDYQ+rU1K48EmXHkSh2Pb9k=";
+      sha256 = "sha256-AV3bqtWaoy6AVnt8K/Qo+7hguAIsPJPZhgLSeOvJ7JY=";
     };
   });
 }
