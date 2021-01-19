d17e7232c067b1cd5ea83ff3761c097989b6ef1b
GTrunSec <gtrunsec@hardenedlinux.org>
2021-01-18 23:45:22 -0800

    update fcitx5

--- local/input.nix
+++ local/input.nix
@@ -3,8 +3,12 @@
 {
   i18n = {
     inputMethod = {
-     enabled = "fcitx";
+     enabled = "fcitx5";
      fcitx.engines = with pkgs.fcitx-engines; [ cloudpinyin rime];
+     fcitx5.addons = with pkgs; [
+       fcitx5-chinese-addons
+       fcitx5-rime
+     ];
     };
   };
 }
--- overlays/virtualbox.nix
+++
@@ -1,7 +0,0 @@
-final: prev: {
-  virtualbox = prev.virtualbox.overrideAttrs (o: {
-    # patches = o.patches ++ [
-    #   ../local/108285.patch
-    # ];
-  });
-}
