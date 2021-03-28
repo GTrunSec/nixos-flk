32ab36143162effd703dab2d856124e072c90ba4
GTrunSec <gtrunsec@hardenedlinux.org>
2021-03-28 02:50:46 -0700

    remvoe ca-derivation

--- users/profiles/home-packages/default.nix
+++ users/profiles/home-packages/default.nix
@@ -1,6 +1,9 @@
 { config, lib, pkgs, ... }:
 let
-  clean-nix-store = pkgs.writeScriptBin "clean-nix-store" (import ../../bin/clean-nix-store.nix { });
+  clean-nix-store = pkgs.writeScriptBin "clean-nix-store" (import ../../bin/clean-nix-store.nix {
+    home-manager = pkgs.home-manager;
+  });
+
   lsyncd-rsync = pkgs.writeScriptBin "lsyncd-rsync" (import ../../bin/lsyncd-rsync.nix { });
 
   LS_COLORS = pkgs.fetchgit {
