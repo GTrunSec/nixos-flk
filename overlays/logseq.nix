45d9f06528e494e8dab760a2e5f1d9819d2d10d4
GTrunSec <gtrunsec@hardenedlinux.org>
2021-05-25 22:45:00 -0700

    iTue May 25 22:45:02 PDT 2021# Please enter the commit message for your changes. Lines starting

--- overlays/logseq.nix
+++ overlays/logseq.nix
@@ -1,11 +1,11 @@
-let version = "0.0.1";
+let version = "0.0.4";
 in
 final: prev: {
   logseq = prev.logseq.overrideAttrs (_: rec{
     inherit version;
     src = prev.fetchurl {
       url = "https://github.com/logseq/logseq/releases/download/${version}/logseq-linux-x64-${version}.AppImage";
-      sha256 = "sha256-CciWP0ELmjX7tgUJMdHkCjjTJnZ3gwtKVX2dt41eUbY=";
+      sha256 = "sha256-dR2Xz2CVEB7BrgbFIXQGmfyidzWsW3YZNnw3rEJ0T7U=";
     };
   });
 }
