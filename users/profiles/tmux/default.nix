b9a736905c296fe2e18bf03a1c916f6436f18041
GTrunSec <gtrunsec@hardenedlinux.org>
2021-05-30 02:26:49 -0700

    Sun May 30 02:26:50 PDT 2021

--- users/profiles/tmux/default.nix
+++ users/profiles/tmux/default.nix
@@ -7,17 +7,19 @@
     extraConfig = (if pkgs.stdenv.isLinux then ''
     '' else ''
     '') + ''
-      unbind C-a
       set -g prefix C-b
       bind C-b send-prefix
-      set -g mouse on
 
+      #https://github.com/gpakosz/.tmux/issues/53
+      set -gu prefix2
+      unbind C-a
+
+      set -g mouse on
       bind P paste-buffer
       bind-key -T copy-mode-vi v send-keys -X begin-selection
       bind-key -T copy-mode-vi y send-keys -X copy-selection
       bind-key -T copy-mode-vi r send-keys -X rectangle-toggle
 
-      set -g @continuum-restore 'on'
     '';
 
     plugins = with pkgs.tmuxPlugins; [
