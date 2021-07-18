c7ca037e1f6ae859632c2ad4761696b09009158c
GTrunSec <gtrunsec@hardenedlinux.org>
2021-07-17 22:13:01 -0700

    WIP: reorg doom-emacs

--- users/profiles/doom-emacs/default.nix
+++ users/profiles/doom-emacs/default.nix
@@ -1,8 +1,12 @@
 { config, lib, pkgs, ... }:
 let
   updatefont = ''fc-cache -f -v'';
-  updateDoom = ".emacs.d/bin/doom sync";
-  updateInit = "bash .doom.d/bin/emacs.sh";
+  onChange = ''
+    export PATH=${pkgs.emacsNg}/bin/:$PATH
+    cd $HOME/.doom.d/
+    ${pkgs.emacsNg}/bin/emacs -Q -batch -l 'lisp/compile.el'
+    $HOME/.emacs.d/bin/doom sync
+  '';
 in
 {
   home.activation.linkEmacsPrivate = config.lib.dag.entryAfter [ "writeBoundary" ] ''
@@ -20,7 +24,6 @@ in
     ln -sfT "$HOME/.config/nixpkgs/nixos-flk/users/dotfiles/doom-emacs/bin" $HOME/.doom.d/bin
     ln -sfT "$HOME/.config/nixpkgs/nixos-flk/users/dotfiles/doom-emacs/snippets" $HOME/.doom.d/snippets
     ln -sfT "$HOME/.config/nixpkgs/nixos-flk/users/dotfiles/doom-emacs/modules" $HOME/.doom.d/modules
-    ln -sfT "$HOME/.config/nixpkgs/nixos-flk/users/dotfiles/doom-emacs/Makefile" $HOME/.doom.d/Makefile
     fi
 
     if [ ! -d "$HOME/.doom.d/modules/my-code" ];then
@@ -32,20 +35,18 @@ in
   # editors
   home.file.".doom.d/config.org" = {
     source = ../../dotfiles/doom-emacs/config.org;
-    onChange = updateInit;
+    inherit onChange;
   };
 
   home.file.".doom.d/meow.org" = {
     source = ../../dotfiles/doom-emacs/meow.org;
-    onChange = updateInit;
+    inherit onChange;
   };
 
   programs.emacs.enable = true;
 
   programs.emacs.package = (pkgs.emacsNg.override ({ })).overrideAttrs (old: rec {
-    configureFlags = (old.configureFlags or [ ]) ++ [
-      "--with-x-toolkit=lucid"
-    ];
+    configureFlags = (old.configureFlags or [ ]) ++ [ ];
   });
 
   programs.emacs.extraPackages = epkgs: with epkgs;[
