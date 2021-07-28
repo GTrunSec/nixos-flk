577dbe6e08ca6e51f6b43dbed5c92365de48b967
GTrunSec <gtrunsec@hardenedlinux.org>
2021-07-28 12:26:29 -0700

    Bumpup nvfecher

--- pkgs/_sources/generated.nix
+++ pkgs/_sources/generated.nix
@@ -5,10 +5,9 @@
     pname = "HPI";
     version = "0.3.20210220";
     src = fetchurl {
-      sha256 = "0i1fh3lkiwwggr0p9l9nvw8a8lxmgslkd90dxm1h4cqpsbckdvln";
       url = "https://pypi.io/packages/source/H/HPI/HPI-0.3.20210220.tar.gz";
+      sha256 = "0i1fh3lkiwwggr0p9l9nvw8a8lxmgslkd90dxm1h4cqpsbckdvln";
     };
-
   };
   StevenBlack-hosts = {
     pname = "StevenBlack-hosts";
@@ -26,45 +25,41 @@
     pname = "adguardhome";
     version = "v0.106.3";
     src = fetchurl {
-      sha256 = "11p081dqilga61zfziw5w37k6v2r84qynhz2hr4gk8367jck54x8";
       url = "https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.106.3/AdGuardHome_linux_amd64.tar.gz";
+      sha256 = "11p081dqilga61zfziw5w37k6v2r84qynhz2hr4gk8367jck54x8";
     };
-
   };
   chord = {
     pname = "chord";
     version = "0.2.38";
     src = fetchurl {
-      sha256 = "02wzfdpz7sj38smdkwa18hpcnz2s0arf5dg2kjq46jxjqz2y92np";
       url = "https://github.com/PeterDing/chord/releases/download/v0.2.38/chord-0.2.38-linux-x86_64.AppImage";
+      sha256 = "02wzfdpz7sj38smdkwa18hpcnz2s0arf5dg2kjq46jxjqz2y92np";
     };
-
   };
   logseq = {
     pname = "logseq";
     version = "0.2.10";
     src = fetchurl {
-      sha256 = "01zrdx12pvr07v8f24l2kqln5dajvjnl4rqy5f3gywn3l74lx1fm";
       url = "https://github.com/logseq/logseq/releases/download/0.2.10/logseq-linux-x64-0.2.10.AppImage";
+      sha256 = "01zrdx12pvr07v8f24l2kqln5dajvjnl4rqy5f3gywn3l74lx1fm";
     };
   };
   magnetw = {
     pname = "magnetw";
     version = "3.1.1";
     src = fetchurl {
-      sha256 = "07qj758adfx3h6qpcwki68fc5rc0iq0ydn5246qh07bzswcrgvl9";
       url = "https://github.com/xiandanin/magnetW/releases/download/3.1.1/magnetw-3.1.1-linux.AppImage";
+      sha256 = "07qj758adfx3h6qpcwki68fc5rc0iq0ydn5246qh07bzswcrgvl9";
     };
-
   };
   motrix = {
     pname = "motrix";
     version = "1.6.11";
     src = fetchurl {
-      sha256 = "0hsazragk4hf9hh1i1hnqhw48kl3f3sckapwzah06w9ysgn90kdl";
       url = "https://github.com/agalwood/Motrix/releases/download/v1.6.11/Motrix-1.6.11.AppImage";
+      sha256 = "0hsazragk4hf9hh1i1hnqhw48kl3f3sckapwzah06w9ysgn90kdl";
     };
-
   };
   notdeft = {
     pname = "notdeft";
@@ -77,25 +72,22 @@
       leaveDotGit = false;
       sha256 = "064pcb57vg1hpkhg2k13bm9svl0ck0mmgf33q6v12za7vpc4bmkf";
     };
-
   };
   orgparse = {
     pname = "orgparse";
     version = "0.2.4";
     src = fetchurl {
-      sha256 = "0x2dbdjnc9hb2l7hhr1zj78fxglnv81dsn1gsh0h56s5rw930v2v";
       url = "https://pypi.io/packages/source/o/orgparse/orgparse-0.2.4.tar.gz";
+      sha256 = "0x2dbdjnc9hb2l7hhr1zj78fxglnv81dsn1gsh0h56s5rw930v2v";
     };
-
   };
   polar-bookshelf = {
     pname = "polar-bookshelf";
     version = "2.0.103";
     src = fetchurl {
-      sha256 = "093j542l7drl3bn42bnpgmvya2983q22vpydahhv1wdxds2v9jld";
       url = "https://github.com/burtonator/polar-bookshelf/releases/download/v2.0.103/polar-desktop-app-2.0.103-amd64.deb";
+      sha256 = "093j542l7drl3bn42bnpgmvya2983q22vpydahhv1wdxds2v9jld";
     };
-
   };
   promnesia = {
     pname = "promnesia";
@@ -108,6 +100,5 @@
       leaveDotGit = false;
       sha256 = "1v36s0kd0375zv70gpqqdk0ngx4m98jjcwa3kpgbsbmmpvqvch95";
     };
-
   };
 }
