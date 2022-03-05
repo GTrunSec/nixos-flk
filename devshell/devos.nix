0048d2dfcaa6116460688fd9b0c3c08e0875ae96
GTrunSec <gtrunsec@hardenedlinux.org>
2022-03-05 14:48:49 -0800

    devshell: cleanup

--- devshell/devos.nix
+++ devshell/devos.nix
@@ -15,28 +15,11 @@ in {
   imports = ["${extraModulesPath}/git/hooks.nix"];
   git = {inherit hooks;};
 
-  packages = with pkgs; [
-    nixopsUnstable
-  ];
+  packages = with pkgs; [];
 
   commands = with pkgs;
     [
       (devos nixUnstable)
-      # {
-      #   category = "devos";
-      #   name = pkgs.nvfetcher-bin.pname + "-vscode";
-      #   help = pkgs.nvfetcher-bin.meta.description + " to vscode";
-      #   command = "cd $PRJ_ROOT/pkgs; ${pkgs.nvfetcher-bin}/bin/nvfetcher -c ./sources-vscode.toml -o ./_sources_vscode $@";
-      # }
-      # {
-      #   category = "devos";
-      #   name = pkgs.nvfetcher-bin.pname;
-      #   help = pkgs.nvfetcher-bin.meta.description;
-      #   command = "cd $PRJ_ROOT/pkgs; ${pkgs.nvfetcher-bin}/bin/nvfetcher -c ./sources.toml $@";
-      # }
-      (linter nixpkgs-fmt)
-      (linter editorconfig-checker)
-      # (docs python3Packages.grip) too many deps
     ]
     ++ lib.optional
     (pkgs ? deploy-rs)
