dc2684c7c897e22caf2ce19fc19326a5d120023b
GTrunSec <gtrunsec@hardenedlinux.org>
2022-05-31 23:21:13 -0700

    emacs: add pin helm

--- templates/devos/nixos/suites.nix
+++ templates/devos/nixos/suites.nix
@@ -8,66 +8,8 @@ with inputs; rec {
     digga.lib.rakeLeaves ../profiles
     // {
       users = digga.lib.rakeLeaves ../users;
-      # virtualisation = digga.lib.rakeLeaves ../profiles/virtualisation;
-      # registry = digga.lib.rakeLeaves ../profiles/registry;
-      # application = digga.lib.rakeLeaves ../profiles/application;
-      # data = digga.lib.rakeLeaves ../profiles/data;
-      # coding = digga.lib.rakeLeaves ../profiles/coding;
-      # networking = digga.lib.rakeLeaves ../profiles/networking;
-      # searching = digga.lib.rakeLeaves ../profiles/searching;
-      # graphical = digga.lib.rakeLeaves ../profiles/graphical;
-      # secrets = digga.lib.rakeLeaves ../profiles/secrets;
     };
   suites = with profiles; rec {
-    base = [core.nixos users.guangtao users.root];
-    # nixos = [ core.nixos users.nixos users.root ];
-    # nixos_ =
-    #   base
-    #   ++ [
-    #     graphical.nvidia
-    #     graphical.i3
-    #     graphical.xserver
-    #     graphical.xdg
-
-    #     application.base
-    #     application.chat
-    #     application.misc
-
-    #     data.promnesia
-    #     # data.nextcloud
-    #     data.miniflux
-    #     data.syncthing
-    #     #data.database
-
-    #     coding.nix
-    #     coding.go
-    #     coding.common
-    #     coding.library
-    #     coding.haskell
-    #     coding.python
-    #     coding.rust
-    #     coding.tex
-    #     coding.vscode
-    #     coding.node
-
-    #     networking.common
-    #     networking.adguardhome
-    #     networking.adblocking
-
-    #     secrets.sops
-
-    #     searching.hound
-
-    #     fonts
-    #     devices
-
-    #     ssh
-    #     nsm
-    #     virtualisation.podman
-    #     virtualisation.libvirtd
-    #     # virtualisation.oci-containers
-    #     # using extra registry
-    #     #registry.nixos
-    #   ];
+    base = [core.nixos users.admin users.root];
   };
 }
